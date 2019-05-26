dfa2f149ec3fbbf55e924b021d55d608165454cc
Tintin361 <30444189+tintin361@users.noreply.github.com>
2019-05-27 00:36:27 +0200

    New

--- app/src/main/java/com/papple/iconoblast/StatFragment.java
+++ app/src/main/java/com/papple/iconoblast/StatFragment.java
@@ -2,6 +2,7 @@ package com.papple.iconoblast;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,16 +10,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Calendar;
 
 public class StatFragment extends Fragment {
+    private TextView timeTextView;
+    private CountDownTimer countDownTimer;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v= inflater.inflate(R.layout.fragment_stat, container, false);
+        final View v= inflater.inflate(R.layout.fragment_stat, container, false);
 
         SharedPreferences settings = getActivity().getSharedPreferences("Answers", 0);
         SharedPreferences.Editor editor = settings.edit();
@@ -33,26 +37,78 @@ public class StatFragment extends Fragment {
 
         editor.apply();
 
-
         Button check = v.findViewById(R.id.giftButton);
         check.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 timeDay();
-            }
-        });
-
-        Button point = v.findViewById(R.id.pointButton);
-        point.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
                 SharedPreferences timePref = getActivity().getSharedPreferences("Time", 0);
                 int value = timePref.getInt("value", 0);
-                Toast.makeText(getActivity(), "Vous avez " + value + " coin(s).", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "Vous avez " + value + " coin(s).", Toast.LENGTH_LONG).show();
             }
         });
 
+        final Thread thread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        Thread.sleep(1000);
+
+                        if(getActivity() == null)
+                            return;
+
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                update();
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        thread.start();
+
         return v;
+    }
+
+    public void update() {
+        Calendar c = Calendar.getInstance();
+        int hours = c.get(Calendar.HOUR_OF_DAY);
+        int minutes = c.get(Calendar.MINUTE);
+        int seconds = c.get(Calendar.SECOND);
+
+        hours = 24 - hours;
+        minutes = 60 - minutes;
+        seconds = 60 - seconds;
+
+        String heure = String.valueOf(hours);
+        String minute = String.valueOf(minutes);
+        String seconde = String.valueOf(seconds);
+
+        if (hours < 10) {
+            heure = "0" + hours;
+
+        } if (minutes < 10) {
+            minute = "0" + minutes;
+
+        } if (seconds < 10) {
+            seconde = "0" + seconds;
+        }
+
+
+        String string = heure + ":" + minute + ":" + seconde;
+
+        if(getActivity() == null)
+            return;
+
+        TextView textView = getActivity().findViewById(R.id.timeTextView);
+        textView.setText(string);
     }
 
     public void timeDay() {
@@ -71,8 +127,6 @@ public class StatFragment extends Fragment {
             value = value + 1;
             editor.putInt("value", value);
             editor.apply();
-        } else {
-            Toast.makeText(getActivity(), "Vous avez déjà fait un CHECK IN !", Toast.LENGTH_SHORT).show();
         }
     }
 }
